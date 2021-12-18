201eff00cc4b1b4a7edf6b7b7452a2fa0a92e578
KritinMathur <kritinmathur75@gmail.com>
2021-12-18 06:16:04 +0530

    added distance calculator, support for 5 UAV

--- src/MAV_model.py
+++ src/MAV_model.py
@@ -3,7 +3,7 @@ import time
 from spade.agent import Agent
 from spade.message import Message
 from spade.template import Template
-from spade.behaviour import CyclicBehaviour,OneShotBehaviour
+from spade.behaviour import CyclicBehaviour, OneShotBehaviour
 from spade_pubsub import PubSubMixin
 import json
 import argparse
@@ -13,51 +13,62 @@ from spade_bdi.bdi import BDIAgent
 import asyncio
 import zmq
 import zmq.asyncio
+import geospatial
 
-HETRO_CHAR = { 'socialization' : 10, 'processing_power' : 20 }
+HETRO_CHAR = {'socialization': 10, 'processing_power': 20}
 
-class MAVAgent(PubSubMixin,BDIAgent):
+
+class MAVAgent(PubSubMixin, BDIAgent):
 
     class MAVtoMAV(CyclicBehaviour):
 
         class Handle_Inform(CyclicBehaviour):
             async def run(self):
-                #Calculate distance between participant & initiator
-                msg = await self.receive(timeout=20)
-                if msg is not None and mav.role == 'initiator':
-                    telem_unit_participant = msg.body
-                    telem_unit_participant = json.loads(telem_unit_participant)
-
-                    #Get telemetry from controller
-                    await mav.socket.send(b'get_telemetry')
-                    telem_unit_initiator = await mav.socket.recv()
-                    telem_unit_initiator = telem_unit_initiator.decode()
-                    telem_unit_initiator = json.loads(telem_unit_initiator)
-
-                    distance_bw_ini_part = telem_unit_initiator['pos']['alt_rel'] - telem_unit_participant['pos']['alt_rel']
-                    print(distance_bw_ini_part)
                 
+                msg = await self.receive()
+                if msg is not None and mav.role == 'initiator':
+
+                    distance_bw_ini_part = float(msg.body)
+                    print("Distance between {} and {} = {} ".format(args.name,msg.sender,distance_bw_ini_part))
+
                 await asyncio.sleep(1)
 
         class Handle_Request(CyclicBehaviour):
             async def run(self):
-                #Getting location of participant
-                msg = await self.receive(timeout=20)
-                    
+                # Getting location of participant
+                msg = await self.receive()
+
                 if msg is not None and mav.role == 'participant':
-                    print(msg.body)
-                    #Get telemetry from controller
+
+                    telem_unit_initiator = msg.body
+                    telem_unit_initiator = json.loads(telem_unit_initiator)
+
+                    # Get telemetry from controller
                     await mav.socket.send(b'get_telemetry')
-                    telem_unit = await mav.socket.recv()
-                    telem_unit = telem_unit.decode()
-                    
+                    telem_unit_participant = await mav.socket.recv()
+                    telem_unit_participant = telem_unit_participant.decode()
+                    print(telem_unit_participant)
+                    telem_unit_participant = json.loads(telem_unit_participant)
+                    print(telem_unit_participant)
 
-                    msg = Message(to=f"{mav.counter_party}@{args.server}")
-                    msg.set_metadata("performative", "inform")
-                    msg.body = telem_unit
+                    lat1 = telem_unit_initiator['pos']['lat']
+                    lon1 = telem_unit_initiator['pos']['lon']
+                    lat2 = telem_unit_participant['pos']['lat']
+                    lon2 = telem_unit_participant['pos']['lon']
 
-                    print('Sending inform from {} to {} '.format(args.name,mav.counter_party))
-                    await self.send(msg)
+                    # Calculate distance between participant & initiator
+                    distance_bw_ini_part = geospatial.distance(lat1,lat2,lon1,lon2)
+                    #print(distance_bw_ini_part)
+
+                    if distance_bw_ini_part <= 150:
+                        
+                        sender_JID = msg.sender
+                        msg = Message(to=f"{sender_JID}@{args.server}")
+                        msg.set_metadata("performative", "inform")
+                        msg.body = str(distance_bw_ini_part)
+
+                        print('Sending inform from {} to {} '.format(args.name, sender_JID))
+                        await self.send(msg)
 
                 await asyncio.sleep(1)
 
@@ -69,14 +80,24 @@ class MAVAgent(PubSubMixin,BDIAgent):
         async def run(self):
             print("Counter: {}".format(self.counter))
             self.counter += 1
-            
+
             if mav.role == 'initiator' and self.counter % 10 == 0:
                 print(mav.role)
-                msg = Message(to=f"{mav.counter_party}@{args.server}")
-                msg.set_metadata("performative", "request")
-                msg.body = "send_location"  
-                print('Sending request from {} to {} '.format(args.name,mav.counter_party))
-                await self.send(msg)   
+                
+                # Get telemetry from controller
+                await mav.socket.send(b'get_telemetry')
+                telem_unit_initiator = await mav.socket.recv()
+                telem_unit_initiator = telem_unit_initiator.decode()
+                telem_unit_initiator = json.loads(telem_unit_initiator) 
+            
+                for counter_party in mav.counter_parties:
+
+                    msg = Message(to=f"{counter_party}@{args.server}")
+                    msg.set_metadata("performative", "request")
+                    msg.body = json.dumps(telem_unit_initiator)
+
+                    print('Sending request from {} to {} '.format(args.name, counter_party))
+                    await self.send(msg)
 
             await asyncio.sleep(1)
 
@@ -91,7 +112,7 @@ class MAVAgent(PubSubMixin,BDIAgent):
             self.counter += 1
 
             command_payload = await mav.pubsub.get_items('pubsub.localhost', "Cmd_node")
-            
+
             if command_payload:
                 command_gcs_literal = command_payload[-1].data
                 command_gcs = json.loads(command_gcs_literal)
@@ -102,57 +123,65 @@ class MAVAgent(PubSubMixin,BDIAgent):
 
                     if command_gcs['data'] == 'mission':
                         print('Starting mission')
-                        
-                        mav.bdi.set_belief('go_mission','positive')
+
+                        mav.bdi.set_belief('go_mission', 'positive')
 
                     if command_gcs['data'] == 'rtl':
                         print('RTL')
-                        
-                        mav.bdi.set_belief('rtl','positive')
+
+                        mav.bdi.set_belief('rtl', 'positive')
 
                     if command_gcs['data'] == 'get_info':
                         print('Sending telemetry data')
 
-                        
                         await mav.socket.send(b'get_telemetry')
                         telem_unit = await mav.socket.recv()
                         telem_unit = telem_unit.decode()
                         telem_unit = json.loads(telem_unit)
                         print(telem_unit)
 
-                        payload = {'ID' : 'test', 'data' : { 'telem' : telem_unit , 'characteristic' : HETRO_CHAR } }
+                        payload = {'ID': 'test', 'data': {
+                            'telem': telem_unit, 'characteristic': HETRO_CHAR}}
                         await mav.pubsub.purge('pubsub.localhost', "Telemetry_node")
                         await mav.pubsub.publish('pubsub.localhost', "Telemetry_node", json.dumps(payload))
 
-
             await asyncio.sleep(1)
-
 
     async def setup(self):
 
-        #ROLE DEFINTION
+        # ROLE DEFINTION
         if args.name == 'test':
             self.role = 'initiator'
-            self.counter_party = 'test2'
-        elif args.name == 'test2':
+            self.counter_parties = ('test2','test3','test4','test5')
+            self.presence.approve_all = True
+
+        elif args.name == 'test2' or args.name == 'test3' or args.name == 'test4' or args.name == 'test5':
             self.role = 'participant'
-            self.counter_party = 'test'
+            self.counter_parties = ('test',)
+            self.approve_all = True
+            
         else:
             self.role == None
             self.counter_party = None
+        
+        #Neighbour definition
+        for counter_party in self.counter_parties:
+            self.presence.subscribe(counter_party)
 
-        #Drone Model-Controller socket 
+        print("neighbor list" ,self.presence.get_contacts())
+        print("number of neighbors",len(self.presence.get_contacts()))
+
+        # Drone Model-Controller socket
         self.context = zmq.asyncio.Context()
         print("Starting drone model controller server…")
         self.socket = self.context.socket(zmq.PAIR)
         self.socket.bind(f"tcp://*:{args.mc_port}")
 
-
-        #Adding behaviours
+        # Adding behaviours
 
         print("GCS Agent starting . . .")
         m2g = self.MAVtoGCS()
-        #self.add_behaviour(m2g)
+        # self.add_behaviour(m2g)
 
         print("IMAV Agent starting . . .")
         m2m = self.MAVtoMAV()
@@ -168,10 +197,10 @@ class MAVAgent(PubSubMixin,BDIAgent):
         req_behave = self.MAVtoMAV.Handle_Request()
         inf_behave = self.MAVtoMAV.Handle_Inform()
 
-        if self.role == 'participant' :
+        if self.role == 'participant':
             self.add_behaviour(req_behave, request_template)
 
-        if self.role == 'initiator' :
+        if self.role == 'initiator':
             self.add_behaviour(inf_behave, inform_template)
 
     ####BDI###
@@ -179,13 +208,13 @@ class MAVAgent(PubSubMixin,BDIAgent):
 
         @actions.add_function(".mission", (int,))
         def _my_function(x):
-            
+
             asyncio.ensure_future(mav.socket.send(b'do_mission'))
             return x
 
         @actions.add_function(".rtl", (int,))
         def _my_function(x):
-            
+
             asyncio.ensure_future(mav.socket.send(b'do_rtl'))
             return x
 
@@ -195,25 +224,33 @@ class MAVAgent(PubSubMixin,BDIAgent):
             print(arg)
             yield
 
+
 if __name__ == "__main__":
 
     global args
     parser = argparse.ArgumentParser(description='MAV_Model BDI')
-    parser.add_argument('--server', type=str, default="localhost", help='XMPP server address.')
-    parser.add_argument('--name', type=str, default="test", help='XMPP name for the agent.')
-    parser.add_argument('--password', type=str, default="password", help='XMPP password for the agent.')
-    parser.add_argument('--autopilot', type=str, default="px4", help='Agent autopilot software.')
-    parser.add_argument('--uav_add', type =str, default='udp://:14540',help='UAV system address')
-    parser.add_argument('--uav_port', type =int, default=50040, help='MAVSDK Server port (only required for PX4)')
-    parser.add_argument('--mc_port', type =int, default=5555, help = 'model controller port')
+    parser.add_argument('--server', type=str,
+                        default="localhost", help='XMPP server address.')
+    parser.add_argument('--name', type=str, default="test",
+                        help='XMPP name for the agent.')
+    parser.add_argument('--password', type=str,
+                        default="password", help='XMPP password for the agent.')
+    parser.add_argument('--autopilot', type=str, default="px4",
+                        help='Agent autopilot software.')
+    parser.add_argument('--uav_add', type=str,
+                        default='udp://:14540', help='UAV system address')
+    parser.add_argument('--uav_port', type=int, default=50040,
+                        help='MAVSDK Server port (only required for PX4)')
+    parser.add_argument('--mc_port', type=int, default=5555,
+                        help='model controller port')
     args = parser.parse_args()
 
-    
     if args.autopilot == 'px4':
-        os.system('python3 src/MAV_controller_px4.py --uav_add {} --uav_port {} --mc_port {} &'.format(args.uav_add, args.uav_port,args.mc_port))
-    
+        os.system('xterm -e python3 src/MAV_controller_px4.py --uav_add {} --uav_port {} --mc_port {} &'.format(
+            args.uav_add, args.uav_port, args.mc_port))
 
-    mav = MAVAgent("{}@{}".format(args.name, args.server), args.password,'./src/behave.asl')
+    mav = MAVAgent("{}@{}".format(args.name, args.server),
+                   args.password, './src/behave.asl')
     future = mav.start()
     future.result()
 
